81bf0c9853545bfe39e1ea5e43c266016904427b
Pete Graham <petegraham1@gmail.com>
2012-03-08 14:46:09 +0000

    monkey module

---
+++ monkey.js
@@ -0,0 +1,9 @@
+var monkey = {
+  say: function (line) {
+    console.log('monkey ' + line);
+  },
+  do: function () {
+    console.log('monkey did a big poo');
+  }
+};
+module.exports = monkey;
---
+++ monkey_test.js
@@ -0,0 +1,5 @@
+var monkey = require('./monkey');
+console.log(monkey);
+
+monkey.say("Hi there little monkey");
+monkey.do();
