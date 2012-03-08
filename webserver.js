67851bcec4a10f54fe2348f435a2449c8cb381f7
Pete Graham <petegraham1@gmail.com>
2012-03-08 11:56:40 +0000

    comments

--- webserver.js
+++ webserver.js
@@ -3,6 +3,7 @@ var http = require('http');
 
 // annoymous callback function
 var server = http.createServer(function(req, res) {
+	// could add extra headers and such here
 	res.end('Non-blocking rockstar tech');
 	console.log(req);
 });
